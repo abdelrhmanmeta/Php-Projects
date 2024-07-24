dd5dfa0931e5e8530834a171c8c5330ea3efc539
abdelrhmanmeta <132745439+abdelrhmanmeta@users.noreply.github.com>
2024-07-24 10:42:41 +0300

    online

---
+++ onlineexamsystem/videoportal/index.php
@@ -0,0 +1,125 @@
+<?php include('allhead.php'); ?>
+
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item">
+			<div class="fill" style="background-image:url('images/1707.jpg');"></div>
+			<div class="carousel-caption">
+				<h2 ><mark> Making Learning Easy</mark></h2>
+			</div>
+		</div>
+		<div class="item active">
+			<div class="fill" style="background-image:url('images/1900x10800.jpg');"></div>
+			<div class="carousel-caption">
+				<a href="registrationform"><h2 style="color: white;"><mark>Register Now</mark></h2></a>
+			</div>
+		</div>
+		<div class="item">
+			<div class="fill" style="background-image:url('images/198989.jpg');"></div>
+			<div class="carousel-caption">
+				<a href="takeassessment"><h2 style="color: white; "><mark>Take Your Lessons</mark></h2>
+                </div>
+            </div>
+        </div>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+			
+</header>
+
+<!-- Page Content -->
+<div class="container" style="max-width: 1200px;">
+
+	<!-- Marketing Icons Section -->
+	<div class="row">
+		<div class="col-lg-12 text-center">
+			<h1 class="page-header">
+                    Welcome to Online video Classroom
+                </h1>
+		
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-check"></i> About Us</h4>
+				</div>
+				<div class="panel-body">
+					<ul style="list-style-type:disc">
+						<li>
+							<p>made by CHI Team For Graduation project 
+						</p>
+					</li>
+					
+					</ul>
+						
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-gift"></i> Objectives</h4>
+				</div>
+				<div class="panel-body">
+					<p>
+					<ul style="list-style-type:disc">
+						<li>To make the institute truly a paperless institute.</li>
+						<li>To learn from by watching videos (e-learn).
+						</li>
+						<li>To make the institute truly a Cloud Based Institute.
+						</li>
+						<li>For reducing manual work and mental conflict.<br></li>
+
+						</ul>
+					 </p>
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-compass"></i> Our Team Names</h4>
+				</div>
+				<div class="panel-body">
+					<p>
+					<ul style="list-style-type:disc">
+					<li>Ahmed Ramdan</li>
+					<li>Name2</li>
+					<li>Name3</li>
+					<li>Name4</li>
+					<li>Name5</li>
+					</ul>
+					</p>
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /.row -->
+
+	
+	<!-- /.row -->
+
+	
+	
+
+
+	<hr>
+
+
+	<?php include('allfoot.php'); ?>
